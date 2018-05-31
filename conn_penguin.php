e11313ff5a4a7a62d9e7349e4c6513c7fe7bdc13
tinydalek <estajt@gmail.com>
2018-05-31 21:57:48 +1000

    Update server connection

--- conn_penguin.php
+++ conn_penguin.php
@@ -2,7 +2,7 @@
 function doDB() {
 	global $mysqli;
 
-	$mysqli = mysqli_connect('localhost','root','','manlypenguin');
+	$mysqli = mysqli_connect('manlyvolunteerpenguinwardens.mysql.database.azure.com','tinydalek@manlyvolunteerpenguinwardens','Classproject1','manlypenguin');
 	if (mysqli_connect_errno()){
 		printf ("Connect failed: %s\n" . mysqli_connect_error());
 		exit();
--- manlyPenguin.sql
+++ manlyPenguin.sql
@@ -1,7 +1,9 @@
 
 CREATE DATABASE manlypenguin;
 
---from SQL tab, copy following and GO!
+Use manlypenguin;
+
+-- from SQL tab, copy following and GO!
 CREATE TABLE donation  (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 donation_name VARCHAR (100),
@@ -23,13 +25,13 @@ enquiry_type VARCHAR(100),
 contact_comments VARCHAR(255)
 );
 
-INSERT INTO 'donation' VALUES (1, 'Tom', '123 Ultimo Rd', 'ultimo', 'NSW', 'tom@tmail.com', '1234543210', '20', 'Paypal', 'I am happy to donate');
+INSERT INTO donation VALUES (1, 'Tom', '123 Ultimo Rd', 'ultimo', 'NSW', 'tom@tmail.com', '1234543210', '20', 'Paypal', 'I am happy to donate');
 
---from SQL tab, copy following and GO!
+-- from SQL tab, copy following and GO!
 -- ALTER TABLE 'donation' ADD 'member_id' INT NULL AFTER 'id';
 -- ALTER TABLE 'contact' ADD 'member_id' INT NULL AFTER 'id'; 
 -- drop table donation; --delete data and table
 -- drop table contact; --delete data and table
 
-SELECT * FROM 'donation';
-SELECT * FROM 'contact';
+SELECT * FROM donation;
+SELECT * FROM contact;
